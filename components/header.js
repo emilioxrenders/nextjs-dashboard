028574117c0d25c93a89156d18578f37dc2f5da3
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-05-16 23:59:39 +0200

    code cleanuo

--- components/header.js
+++ components/header.js
@@ -1,5 +1,3 @@
 export default function Header() {
-    return <div className="bg-black">
-        <p className="text-white w-fit mx-auto text-center font-light py-3">Pexels Project</p>
-    </div>;
+    return <div className="bg-black"><p className="text-white w-fit mx-auto text-center font-light py-3">Pexels Project</p></div>;
 }
