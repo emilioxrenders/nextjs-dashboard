d25758207e14241a7e3caf7d923648d49a53d817
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-05-16 23:59:16 +0200

    code cleanup

--- components/header.js
+++ components/header.js
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function Header() {
     return <div className="bg-black">
         <p className="text-white w-fit mx-auto text-center font-light py-3">Pexels Project</p>
