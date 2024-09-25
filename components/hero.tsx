d5a4abdba77fcf70fb57d5d60fe99dd6033a111d
Emilio Renders <emilioxrenders@Emilios-MacBook-Air.local>
2024-09-25 16:25:07 +0200

    update

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,7 +4,6 @@ export default function Hero() {
   return (
     <div className="relative container mx-auto px-10 py-20 w-full flex items-center justify-center font-display font-light">
       <div className="flex flex-col gap-3 text-center">
-        <h1 className="text-2xl">Next.js Cryptocurrency Forecast Dashboard</h1>
         <p>This project displays a list of predictions.</p>
       </div>
     </div>
